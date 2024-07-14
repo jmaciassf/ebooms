561a28f341c443ea0d6c272d61a75f6cfe3fa5bc
Jesus Macias <jesus.macias.sifuentes@banorte.com>
2024-07-14 02:41:27 -0600

    animation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     console.log("ready");
 
-
     $(".section1 .left, .section1 .flex3").fadeOut();
     setTimeout(function () {
         $(".section1 .left, .section1 .flex3, .section1 .line2").fadeIn(1000);
@@ -106,12 +105,66 @@ function startNumbers(){
     }
 }
 
+let positions = {}
 function resize() {    
     $(".step1 .bomb").css("marginTop", $(".step1 .box").height() / 2 - 30);
     $(".step2 .bomb").css("marginTop", $(".step2 .box").height() / 2 - 30);
     $(".step3 .bomb").css("marginTop", $(".step3 .box").height() / 2 - 30);
     //$(".steps").css("marginBottom", $(".step3 .box").height() / 2 - 30);
+
+    positions.section2 = $(".divSection2").position().top - 450;
+    positions.reserve = $(".divReserve").position().top - 450;
+    positions.aboutUs = $(".aboutUs").position().top - 450;
+    
 }
+
+$(window).scroll(function(){
+    var scrollTop = $(window).scrollTop();
+    if(scrollTop > positions.aboutUs)
+    {
+        // Show reserve
+        if(!$(".divAboutUs").hasClass("show")){
+            $(".divAboutUs").addClass("show");
+        }
+        hideSection2();
+    }
+    else if(scrollTop > positions.reserve)
+    {
+        // Show reserve
+        if(!$(".divReserve").hasClass("show")){
+            $(".divReserve").addClass("show");
+        }
+    }
+    else if(scrollTop > positions.section2)
+    {
+        // Show section2
+        if(!$(".divSection2").hasClass("show")){
+            $(".divSection2").addClass("show");
+            startCarousel();
+        }
+        hideAboutUs();
+    }
+    else 
+    {
+        hideSection2();
+        if($(".divReserve").hasClass("show")){
+            $(".divReserve").removeClass("show");
+        }
+        hideAboutUs();
+    }
+
+    function hideSection2() {
+        if($(".divSection2").hasClass("show")){
+            $(".divSection2").removeClass("show");
+            stopCarousel();
+        }
+    }
+    function hideAboutUs() {
+        if($(".divAboutUs").hasClass("show")){
+            $(".divAboutUs").removeClass("show");
+        }
+    }
+});
 
 function call(){
     window.open("https://calendly.com/joshuasanchez-ebooms/llamada-demo?month=2024-07");
